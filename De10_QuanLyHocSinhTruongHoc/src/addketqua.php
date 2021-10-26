59d997d0aad529edf144f6671c3218de3b51cdfc
khiem <khiem22072001@gmail.com>
2021-10-26 23:23:33 +0700

    sưa add ket qua

--- De10_QuanLyHocSinhTruongHoc/src/addketqua.php
+++ De10_QuanLyHocSinhTruongHoc/src/addketqua.php
@@ -8,7 +8,7 @@
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <title>addketqua</title>
 </head>
 
@@ -53,6 +53,8 @@
                             </tr>
                             <?php
                             include('connect.php');
+                            $sql = "SELECT * FROM KETQUA";
+                            $result = mysqli_query($conn, $sql);
                             while ($row = mysqli_fetch_array($result)) {
                             ?>
                                 <tr>
