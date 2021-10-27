770a2afab91e930ef15bf18f2e895a55b403f6bf
quoctien01 <quoctienx10@gmail.com>
2021-10-27 15:31:53 +0700

    tao trang addphuhuynh

---
+++ De10_QuanLyHocSinhTruongHoc/src/addphuhuynh.php
@@ -0,0 +1,168 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <title>addphuhuynh</title>
+</head>
+
+<body>
+    <div class="container">
+
+        <div class="row">
+            <div class="col-md-12">
+
+
+                <h2 align="center">Import Phụ Huynh</h2>
+                <br>
+                <form id="upload_csv" method="post" enctype="multipart/form-data">
+                    <div class="col-md-3">
+                        <br />
+                        <label>Add More Data</label>
+                    </div>
+                    <div class="col-md-4">
+                        <input type="file" name="employee_file" style="margin-top:15px;" />
+                    </div>
+                    <div class="col-md-5">
+                        <input type="submit" name="upload" id="upload" value="Upload" style="margin-top:10px;"
+                            class="btn btn-info" />
+                    </div>
+                    <div style="clear:both"></div>
+                </form>
+                <br /><br /><br />
+                <form action="processphuhuynh.php" method="POST">
+                    <div class="form-group">
+                        <label for="exampleInputMaHS">MaHS </label>
+                        <input type="text" name ="MaHS" class="form-control" id="exampleInputMaHS" aria-describedby=""
+                           >
+                        
+                    </div>
+                   
+                    <div class="form-group">
+                        <label for="exampleInputGioiTinh">Tên Học sinh </label>
+                        <input type="text" name ="TenHS" class="form-control" id="exampleInputGioiTinh" aria-describedby=""
+                            >
+                        
+                    </div>
+                    <div class="form-group">
+                        <label for="exampleInputTenLop">Tên lớp </label>
+                        <input type="Text" name ="TenLop" class="form-control" id="exampleInputTenLop" aria-describedby=""
+                            >
+                       
+                    </div>
+                    <div class="form-group">
+                        <label for="exampleInputNgaySinh">Tên Bố </label>
+                        <input type="Text" name ="TenBo" class="form-control" id="exampleInputTenBo" aria-describedby=""
+                       >
+                    </div>
+                    <div class="form-group">
+                        <label for="exampleInputDiaChi">Tên mẹ</label>
+                        <input type="Text" name ="TenMe" class="form-control" id="exampleInputTenMe" aria-describedby=""
+                            >
+                    </div>
+                    <div class="form-group">
+                        <label for="exampleInputDiaChi">Email</label>
+                        <input type="Text" name ="Email" class="form-control" id="exampleInputDiaChi" aria-describedby=""
+                           >
+                    </div>
+                    <div class="form-group">
+                        <label for="exampleInputSDT">SDT</label>
+                        <input type="text" name ="SDT" class="form-control" id="exampleInputSDT" aria-describedby=""
+                            >
+                    </div>
+                    <div class="form-group">
+                        <label for="exampleInputSDT">Địa chỉ</label>
+                        <input type="text" name ="Diachi" class="form-control" id="exampleInputSDT" aria-describedby=""
+                          >
+                    </div>
+                    <button type="submit" name ="btnPhuHuynh"class="btn btn-primary">Thêm Phụ Huynh</button>
+                </form>
+                <br /><br /><br />
+                <div class="table-responsive" id="employee_table">
+                    <table class="table table-bordered">
+                        <tr>
+                            <th width="">STT</th>
+                            <th width="">MaHS</th>
+                            <th width="">TenHs</th>
+                            <th width="">TenLop</th>
+                            <th width="">TenBo</th>
+                            <th width="">TenMe</th>
+                            <th width="">Email</th>
+                            <th width="">SDT</th>
+                            <th width="">DiaChi</th>
+                            
+                        </tr>
+                        <?php
+                            include('connect.php');
+                            $sql = "SELECT * FROM PHUHUYNH";
+                            $result = mysqli_query($conn, $sql);
+                            while ($row = mysqli_fetch_array($result)) {
+                            ?>
+                        <tr>
+                            <td><?php echo $row["STT"]; ?></td>
+                            <td><?php echo $row["MaHS"]; ?></td>
+                            <td><?php echo $row["TenHS"]; ?></td>
+                            <td><?php echo $row["TenLop"]; ?></td>
+                            <td><?php echo $row["TenBo"]; ?></td>
+                            <td><?php echo $row["TenMe"]; ?></td>
+                            <td><?php echo $row["Email"]; ?></td>
+                            <td><?php echo $row["SDT"]; ?></td>
+                            <td><?php echo $row["DiaChi"]; ?></td>
+                            
+
+                        </tr>
+                        <?php
+                            }
+                            ?>
+                    </table>
+                </div>
+
+            </div>
+        </div>
+    </div>
+    <script>
+    $(document).ready(function() {
+        $('#upload_csv').on("submit", function(e) {
+            e.preventDefault(); //form will not submitted  
+            $.ajax({
+                url: "importphuhuynh.php",
+                method: "POST",
+                data: new FormData(this),
+                contentType: false, // The content type used when sending data to the server.  
+                cache: false, // To unable request pages to be cached  
+                processData: false, // To send DOMDocument or non processed data file it is set to false  
+                success: function(data) {
+                    if (data == 'Error1') {
+                        alert("Invalid File");
+                    } else if (data == "Error2") {
+                        alert("Please Select File");
+                    } else {
+                        $('#employee_table').html(data);
+                    }
+                }
+            })
+        });
+    });
+    </script>
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
+    </script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+    -->
+</body>
+
+</html>
