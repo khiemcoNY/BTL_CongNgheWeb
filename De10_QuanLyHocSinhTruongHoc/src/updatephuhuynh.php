131fd2a7b837898331803ec79f10491027c297dd
quoctien01 <quoctienx10@gmail.com>
2021-10-29 11:14:10 +0700

    bo sung bang update ketqua va phuhuynh

--- De10_QuanLyHocSinhTruongHoc/src/updatephuhuynh.php
+++ De10_QuanLyHocSinhTruongHoc/src/updatephuhuynh.php
@@ -13,6 +13,36 @@
 </head>
 
 <body>
+<?php
+        $TenHS= "";
+        $TenLop= "";
+        $TenBo= "";
+        $TenMe= "";
+        $Email= "";
+        $SDT= "";
+        $DiaChi= "";
+       
+       
+
+    if(isset($_GET['idsua'])){
+        include('connect.php');
+        $E= $_GET['idsua'];
+        $sql = "SELECT * FROM PHUHUYNH where MaHS='$E'";
+        $result = mysqli_query($conn, $sql);
+        if(mysqli_num_rows($result) >0){
+            $row = mysqli_fetch_assoc($result);
+            $TenHS= $row["TenHS"];
+            $TenLop= $row["TenLop"];
+            $TenBo= $row["TenBo"];
+            $TenMe= $row["TenMe"];
+            $Email= $row["Email"];
+            $SDT= $row["SDT"];
+            $DiaChi= $row["DiaChi"];
+           
+            
+        }   
+    }
+    ?>
     <div class="container">
         <div class="row">
             <div class="col-md-12">
@@ -27,40 +57,40 @@
                     <div class="form-group">
                         <label for="exampleInputTenHS">Tên Học Sinh </label>
                         <input type="text" name ="Up_TenHS" class="form-control" id="exampleInputTenHS" aria-describedby=""
-                           >
+                        value="<?php echo $TenHS?>">
                        
                     </div>
                     <div class="form-group">
-                        <label for="exampleInputGioiTinh">Tên Lớp</label>
+                        <label for="exampleInputTenLop">Tên Lớp</label>
                         <input type="text" name ="Up_TenLop" class="form-control" id="exampleInputGioiTinh" aria-describedby=""
-                            >
+                        value="<?php echo $TenLop?>"  >
                         
                     </div>
                     <div class="form-group">
-                        <label for="exampleInputTenLop">Tên Bố </label>
+                        <label for="exampleInputTenBo">Tên Bố </label>
                         <input type="Text" name ="Up_TenBo" class="form-control" id="exampleInputTenLop" aria-describedby=""
-                           >
+                        value="<?php echo $TenBo?>" >
                        
                     </div>
                     <div class="form-group">
-                        <label for="exampleInputNgaySinh">Tên Mẹ</label>
+                        <label for="exampleInputTenMe">Tên Mẹ</label>
                         <input type="Text" name ="Up_TenMe" class="form-control" id="exampleInputNgaySinh" aria-describedby=""
-                        >
+                        value="<?php echo $TenMe?>">
                     </div>
                     <div class="form-group">
-                        <label for="exampleInputDiaChi">Email</label>
+                        <label for="exampleInputEmail">Email</label>
                         <input type="Text" name ="Up_Email" class="form-control" id="exampleInputDiaChi" aria-describedby=""
-                           >
+                        value="<?php echo $Email?>">
                     </div>
                     <div class="form-group">
                         <label for="exampleInputSDT">SDT</label>
                         <input type="text" name ="Up_SDT" class="form-control" id="exampleInputSDT" aria-describedby=""
-                          >
+                        value="<?php echo $SDT?>">
                     </div>
                     <div class="form-group">
-                        <label for="exampleInputSDT">Địa Chỉ</label>
+                        <label for="exampleInputDiaChi">Địa Chỉ</label>
                         <input type="text" name ="Up_DiaChi" class="form-control" id="exampleInputSDT" aria-describedby=""
-                            >
+                        value="<?php echo $DiaChi?>" >
                     </div>
                     <button type="submit" name ="btnUpPH"class="btn btn-primary">Cập Nhật Thông Tin</button>
                 </form>
