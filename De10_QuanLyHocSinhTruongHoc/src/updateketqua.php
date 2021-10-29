74c73dd1ede54ef26d98ff569e66688e3bde7984
QuangDungVu <quangdungvu629@gmail.com>
2021-10-29 21:54:06 +0700

    css bang ketqua va phuhuynh

--- De10_QuanLyHocSinhTruongHoc/src/updateketqua.php
+++ De10_QuanLyHocSinhTruongHoc/src/updateketqua.php
@@ -7,61 +7,89 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
     <title>admin</title>
 </head>
+<style>
+.style1 {
+    border: 2px solid black;
+    padding: 40px;
+    margin-top: 70px;
+    border-radius: 15px;
+    background-color: #ccc;
 
+}
+
+.style1 h2 {
+    color: white;
+    text-shadow: 1px 1px 2px black, 0 0 40px rgb(34, 34, 36), 0 0 13px rgb(32, 32, 34);
+}
+
+
+</style>
 <body>
     <div class="container">
         <div class="row">
-            <div class="col-md-12">
-            <h2 align="center">Update Kết Quả</h2>
+            <div class="col-md-12 style1">
+                <h2 align="center">Update Kết Quả</h2>
                 <form action="processketqua.php" method="POST">
                     <div class="form-group">
                         <label for="exampleInputMaHS">Mã Học Sinh</label>
-                        <input type="text" name="Up_MaHS" class="form-control" id="exampleInputMaHS"  readonly aria-describedby=""  value ="<?php echo $_GET["idsua"]; ?>" placeholder="Enter Mã Học Sinh">
+                        <input type="text" name="Up_MaHS" class="form-control" id="exampleInputMaHS" readonly
+                            aria-describedby="" value="<?php echo $_GET["idsua"]; ?>" placeholder="Enter Mã Học Sinh"
+                           >
                     </div>
                     <div class="form-group">
                         <label for="exampleInputTenHS">Tên Học Sinh</label>
-                        <input type="text" name="Up_TenHS" class="form-control" id="exampleInputTenHS" aria-describedby="" placeholder="Enter Ten Học Sinh">
+                        <input type="text" name="Up_TenHS" class="form-control" id="exampleInputTenHS"
+                            aria-describedby="" placeholder="Enter Ten Học Sinh" >
                     </div>
                     <div class="form-group">
                         <label for="exampleInputGioiTinh">Giới Tính</label>
-                        <input type="text" name="Up_GioiTinh" class="form-control" id="exampleInputGioiTinh" aria-describedby="" placeholder="Enter Gioi Tinh">
+                        <input type="text" name="Up_GioiTinh" class="form-control" id="exampleInputGioiTinh"
+                            aria-describedby="" placeholder="Enter Gioi Tinh" >
                     </div>
                     <div class="form-group">
                         <label for="exampleInputTenLop">Tên Lớp</label>
-                        <input type="text" name="Up_TenLop" class="form-control" id="exampleInputTenLop" aria-describedby="" placeholder="Enter Tên Lớp">
-                    </div> 
+                        <input type="text" name="Up_TenLop" class="form-control" id="exampleInputTenLop"
+                            aria-describedby="" placeholder="Enter Tên Lớp">
+                    </div>
                     <div class="form-group">
                         <label for="exampleInputDiemVan">Điểm Văn</label>
-                        <input type="text" name="Up_DiemVan" class="form-control" id="exampleInputDiemVan" aria-describedby="" placeholder="Enter Diem">
-                    </div>
-                     <div class="form-group">
-                        <label for="exampleInputDiemToan">Điểm Toán</label>
-                        <input type="text" name="Up_DiemToan" class="form-control" id="exampleInputDiemToan" aria-describedby="" placeholder="Enter Diem Toan">
-                    </div>
-                     <div class="form-group">
-                        <label for="exampleInputDiemAnh">Điểm Anh</label>
-                        <input type="text" name="Up_DiemAnh" class="form-control" id="exampleInputDiemAnh" aria-describedby="" placeholder="Enter Diem Anh">
+                        <input type="text" name="Up_DiemVan" class="form-control" id="exampleInputDiemVan"
+                            aria-describedby="" placeholder="Enter Diem">
                     </div>
                     <div class="form-group">
-              
-                    <label for="exampleInputDiemLy">Điểm Lý</label>
-                        <input type="text" name="Up_DiemLy" class="form-control" id="exampleInputDiemLy" aria-describedby="" placeholder="Enter Diem Ly">
+                        <label for="exampleInputDiemToan">Điểm Toán</label>
+                        <input type="text" name="Up_DiemToan" class="form-control" id="exampleInputDiemToan"
+                            aria-describedby="" placeholder="Enter Diem Toan" >
+                    </div>
+                    <div class="form-group">
+                        <label for="exampleInputDiemAnh">Điểm Anh</label>
+                        <input type="text" name="Up_DiemAnh" class="form-control" id="exampleInputDiemAnh"
+                            aria-describedby="" placeholder="Enter Diem Anh" >
+                    </div>
+                    <div class="form-group">
+
+                        <label for="exampleInputDiemLy">Điểm Lý</label>
+                        <input type="text" name="Up_DiemLy" class="form-control" id="exampleInputDiemLy"
+                            aria-describedby="" placeholder="Enter Diem Ly" >
                     </div>
                     <div class="form-group">
                         <label for="exampleInputDiemHoa">Điểm Hóa</label>
-                        <input type="text" name="Up_DiemHoa" class="form-control" id="exampleInputDiemHoa" aria-describedby="" placeholder="Enter Diem Hoa">
+                        <input type="text" name="Up_DiemHoa" class="form-control" id="exampleInputDiemHoa"
+                            aria-describedby="" placeholder="Enter Diem Hoa" >
                     </div>
                     <div class="form-group">
                         <label for="exampleInputDiemSinh">Điểm Sinh</label>
-                        <input type="text" name="Up_DiemSinh" class="form-control" id="exampleInputDiemSinh" aria-describedby="" placeholder="Enter Diem Sinh">
+                        <input type="text" name="Up_DiemSinh" class="form-control" id="exampleInputDiemSinh"
+                            aria-describedby="" placeholder="Enter Diem Sinh" >
                     </div>
-                   
-                    
-                    <button type="submit" name="btnUpKQ" class="btn btn-primary">Cập Nhật Kết Quả</button>
+
+
+                    <button style="margin-top:10px;" type="submit" name="btnUpKQ" class="btn btn-primary">Cập Nhật Kết Quả</button>
                 </form>
                 <br /><br /><br />
             </div>
@@ -71,7 +99,9 @@
     <!-- Optional JavaScript; choose one of the two! -->
 
     <!-- Option 1: Bootstrap Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
+    </script>
 
     <!-- Option 2: Separate Popper and Bootstrap JS -->
     <!--
--- De10_QuanLyHocSinhTruongHoc/src/updatephuhuynh.php
+++ De10_QuanLyHocSinhTruongHoc/src/updatephuhuynh.php
@@ -9,61 +9,107 @@
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
-    <title>admin</title>
+    <title>Update Phụ huynh</title>
 </head>
+<style>
+.style1 {
+    border: 2px solid black;
+    padding: 40px;
+    margin-top: 70px;
+    border-radius: 15px;
+    background-color: #ccc;
 
+}
+
+.style1 h2 {
+    color: white;
+    text-shadow: 1px 1px 2px black, 0 0 40px rgb(34, 34, 36), 0 0 13px rgb(32, 32, 34);
+}
+
+
+</style>
 <body>
+<?php
+        $MaHS= "";
+        $TenHS= "";
+        $TenLop= "";
+        $TenBo= "";
+        $TenMe= "";
+        $Email= "";
+        $SDT= "";
+        $DiaChi= "";
+        
+    if(isset($_GET['idsua'])){
+        include('connect.php');
+        $E= $_GET['idsua'];
+        $sql = "SELECT * FROM PHUHUYNH where MaHS='$E'";
+        $result = mysqli_query($conn, $sql);
+        if(mysqli_num_rows($result) >0){
+            $row = mysqli_fetch_assoc($result);
+            $MaHS= $row["MaHS"];
+            $TenHS= $row["TenHS"];
+            $TenLop= $row["TenLop"];
+            $TenBo= $row["TenBo"];
+            $TenMe= $row["TenMe"];
+            $Email= $row["Email"];
+            $SDT= $row["SDT"];
+            $DiaChi= $row["DiaChi"];
+            
+        }   
+    }
+    
+    ?>
     <div class="container">
         <div class="row">
-            <div class="col-md-12">
+            <div class="col-md-12 style1">
             <h2 align="center">Update Phụ huynh</h2>
                 <form action="processphuhuynh.php" method="POST">
                     <div class="form-group">
                         <label for="exampleInputMaHS">Mã Học Sinh </label>
                         <input type="text" name ="Up_MaHS" class="form-control" id="exampleInputMaHS"  readonly aria-describedby=""
-                        value ="<?php echo $_GET["idsua"]; ?>">
+                        value ="<?php echo $_GET["idsua"]; ?>" value="<?php echo $MaHS?>">
                         
                     </div>
                     <div class="form-group">
                         <label for="exampleInputTenHS">Tên Học Sinh </label>
                         <input type="text" name ="Up_TenHS" class="form-control" id="exampleInputTenHS" aria-describedby=""
-                           >
+                        value="<?php echo $TenHS?>"  >
                        
                     </div>
                     <div class="form-group">
                         <label for="exampleInputGioiTinh">Tên Lớp</label>
                         <input type="text" name ="Up_TenLop" class="form-control" id="exampleInputGioiTinh" aria-describedby=""
-                            >
+                        value="<?php echo $TenLop?>"   >
                         
                     </div>
                     <div class="form-group">
                         <label for="exampleInputTenLop">Tên Bố </label>
                         <input type="Text" name ="Up_TenBo" class="form-control" id="exampleInputTenLop" aria-describedby=""
-                           >
+                        value="<?php echo $TenBo?>" >
                        
                     </div>
                     <div class="form-group">
                         <label for="exampleInputNgaySinh">Tên Mẹ</label>
                         <input type="Text" name ="Up_TenMe" class="form-control" id="exampleInputNgaySinh" aria-describedby=""
-                        >
+                        value="<?php echo $TenMe?>">
                     </div>
                     <div class="form-group">
                         <label for="exampleInputDiaChi">Email</label>
                         <input type="Text" name ="Up_Email" class="form-control" id="exampleInputDiaChi" aria-describedby=""
-                           >
+                        value="<?php echo $Email?>">
                     </div>
                     <div class="form-group">
                         <label for="exampleInputSDT">SDT</label>
                         <input type="text" name ="Up_SDT" class="form-control" id="exampleInputSDT" aria-describedby=""
-                          >
+                        value="<?php echo $SDT?>" >
                     </div>
                     <div class="form-group">
                         <label for="exampleInputSDT">Địa Chỉ</label>
                         <input type="text" name ="Up_DiaChi" class="form-control" id="exampleInputSDT" aria-describedby=""
-                            >
+                        value="<?php echo $DiaChi?>">
                     </div>
-                    <button type="submit" name ="btnUpPH"class="btn btn-primary">Cập Nhật Thông Tin</button>
-                </form>
+                    <button style="margin-top:10px;" type="submit" name ="btnUpPH"class="btn btn-primary">Cập Nhật Thông Tin</button>
+                </form> 
                 <br /><br /><br />
             </div>
         </div>
