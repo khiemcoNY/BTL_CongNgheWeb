1e8f5653866ffadad6f217980030e202109e39b2
QuangDungVu <quangdungvu629@gmail.com>
2021-10-29 11:17:45 +0700

    bo sung bang giaovien va hocsinh

--- De10_QuanLyHocSinhTruongHoc/src/updategiaovien.php
+++ De10_QuanLyHocSinhTruongHoc/src/updategiaovien.php
@@ -7,40 +7,74 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
     <title>Update GIAOVIEN</title>
 </head>
 
 <body>
+    <?php
+       $TenGV= "";
+       $GioiTinh= "";
+       $NgaySinh= "";
+       $DiaChi= "";
+       $SDT= "";
+        
+    if(isset($_GET['idsua'])){
+        include('connect.php');
+        $E= $_GET['idsua'];
+        $sql = "SELECT * FROM GIAOVIEN where MaGV='$E'";
+        $result = mysqli_query($conn, $sql);
+        if(mysqli_num_rows($result) >0){
+            $row = mysqli_fetch_assoc($result);
+            $TenGV= $row["TenGV"];
+            $GioiTinh= $row["GioiTinh"];
+            $NgaySinh= $row["NgaySinh"];
+            $DiaChi= $row["DiaChi"];
+            $SDT= $row["SDT"];
+
+            
+        }   
+    }
+    
+    ?>
+
     <div class="container">
         <div class="row">
             <div class="col-md-12">
-            <h2 align="center">Update Giáo Viên</h2>
-            <form action="processgiaovien.php" method="POST">
+                <h2 align="center">Update Giáo Viên</h2>
+                <form action="processgiaovien.php" method="POST">
                     <div class="mb-3">
-                        <label for="exampleInputMaGV" >Mã giáo viên</label>
-                        <input type="text" name="Up_MaGV" class="form-control" id="exampleInputMaGV"readonly  aria-describedby="" value="<?php echo $_GET['idsua'] ?>" placeholder="Nhập vào Mã giáo viên" >
+                        <label for="exampleInputMaGV">Mã giáo viên</label>
+                        <input type="text" name="Up_MaGV" class="form-control" id="exampleInputMaGV" readonly
+                            aria-describedby="" value="<?php echo $_GET['idsua'] ?>"
+                            placeholder="Nhập vào Mã giáo viên">
                     </div>
                     <div class="mb-3">
-                        <label for="exampleInputTenGV" >Tên giáo viên</label>
-                        <input type="text" name="Up_TenGV" class="form-control" id="exampleInputTenGV" aria-describedby=""  placeholder="Nhập vào tên giáo viên">
+                        <label for="exampleInputTenGV">Tên giáo viên</label>
+                        <input type="text" name="Up_TenGV" class="form-control" id="exampleInputTenGV"
+                            aria-describedby="" value="<?php echo $TenGV?>" placeholder="Nhập vào tên giáo viên">
                     </div>
                     <div class="mb-3">
-                        <label for="exampleInputGioiTinh" >Giới tính</label>
-                        <input type="text" name="Up_GioiTinh" class="form-control" id="exampleInputGioiTinh" aria-describedby=""  placeholder="Nhập vào giới tính">
+                        <label for="exampleInputGioiTinh">Giới tính</label>
+                        <input type="text" name="Up_GioiTinh" class="form-control" id="exampleInputGioiTinh"
+                            aria-describedby="" value="<?php echo $GioiTinh?>" placeholder="Nhập vào giới tính">
                     </div>
                     <div class="mb-3">
-                        <label for="exampleInputNgaySinh" >Ngày sinh</label>
-                        <input type="text" name="Up_NgaySinh" class="form-control" id="exampleInputNgaySinh" aria-describedby=""  placeholder="Nhập vào ngày sinh">
+                        <label for="exampleInputNgaySinh">Ngày sinh</label>
+                        <input type="text" name="Up_NgaySinh" class="form-control" id="exampleInputNgaySinh"
+                            aria-describedby="" value="<?php echo $NgaySinh?>" placeholder="Nhập vào ngày sinh">
                     </div>
                     <div class="mb-3">
-                        <label for="exampleInputDiaChi" >Địa chỉ</label>
-                        <input type="text" name="Up_DiaChi" class="form-control" id="exampleInputDiaChi" aria-describedby=""  placeholder="Nhập vào địa chỉ">
+                        <label for="exampleInputDiaChi">Địa chỉ</label>
+                        <input type="text" name="Up_DiaChi" class="form-control" id="exampleInputDiaChi"
+                            aria-describedby="" value="<?php echo $DiaChi?>" placeholder="Nhập vào địa chỉ">
                     </div>
                     <div class="mb-3">
-                        <label for="exampleInputSDT" >Số điện thoại</label>
-                        <input type="text" name="Up_SDT" class="form-control" id="exampleInputSDT" aria-describedby="" placeholder="Nhập vào số điện thoại" >
+                        <label for="exampleInputSDT">Số điện thoại</label>
+                        <input type="text" name="Up_SDT" class="form-control" id="exampleInputSDT" aria-describedby=""
+                            value="<?php echo $SDT?>" placeholder="Nhập vào số điện thoại">
                     </div>
                     <button type="submit" name="btnUpGV" class="btn btn-primary">Cập nhật thông tin</button>
                 </form>
@@ -51,7 +85,9 @@
     <!-- Optional JavaScript; choose one of the two! -->
 
     <!-- Option 1: Bootstrap Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
+    </script>
 
     <!-- Option 2: Separate Popper and Bootstrap JS -->
     <!--
