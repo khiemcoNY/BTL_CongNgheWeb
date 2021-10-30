4897a5584dd338e544942b0a922b1c551d119a70
QuangDungVu <quangdungvu629@gmail.com>
2021-10-30 11:38:20 +0700

    fix bug

--- De10_QuanLyHocSinhTruongHoc/src/process-SendMailKQ.php
+++ De10_QuanLyHocSinhTruongHoc/src/process-SendMailKQ.php
@@ -1,4 +1,6 @@
 <?php
 include('connect.php');
-$sql = "SELECT  k.MaHS, k.TenHS, k.TenLop, k.DiemToan, k.DiemVan, k.DiemAnh, k.DiemLy, k.DiemHoa, k.DiemSinh,p.Email FROM KETQUA k, PHUHUYNH p WHERE KETQUA.MaHS=PHUHUYNH.MaHS"
+$sql = "SELECT  k.MaHS, k.TenHS, k.TenLop, k.DiemToan, k.DiemVan, k.DiemAnh, k.DiemLy, k.DiemHoa, k.DiemSinh,p.Email FROM KETQUA k, PHUHUYNH p WHERE KETQUA.MaHS=PHUHUYNH.MaHS";
+$result = mysqli_query; 
+
 ?>
