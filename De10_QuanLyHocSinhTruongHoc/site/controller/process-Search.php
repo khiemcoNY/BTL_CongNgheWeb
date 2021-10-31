30ead3ad39cd9c8b6e71a498ee3dfe3af516f058
khiem <khiem22072001@gmail.com>
2021-10-31 13:31:08 +0700

    fix bug

--- De10_QuanLyHocSinhTruongHoc/site/controller/process-Search.php
+++ De10_QuanLyHocSinhTruongHoc/site/controller/process-Search.php
@@ -1,9 +1,9 @@
 <?php
     include('configs/connect.php');
      $result = mysqli_query($conn, $sql);
-    if($result>0){
+    if(mysqli_num_rows($result) >0){
     $i=1;
-    while($row=mysqli_fetch_array($result)){                             
+    while($row=mysqli_fetch_assoc($result)){                             
     echo "<tr>";
     echo"<td>".$i."</td>";
     echo"<td>".$row["MaHS"]."</td>";
